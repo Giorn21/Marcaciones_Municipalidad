1020129c8b1af4ce88c6a1a0903c6fc60e1eaade
Giorn21 <reyesgiovanni41@gmail.com>
2024-11-04 17:53:34 -0300

    Mejorar el apartado Visual del programa, para no tener cancer a los ojos de ver todo blanco y feo :D.

--- Proyecto/Form_Login.Designer.cs
+++ Proyecto/Form_Login.Designer.cs
@@ -30,17 +30,20 @@
         {
             this.lbl_usuario = new System.Windows.Forms.Label();
             this.lbl_pass = new System.Windows.Forms.Label();
-            this.btn_login = new System.Windows.Forms.Button();
             this.txt_Usuario = new System.Windows.Forms.TextBox();
             this.txt_Contrasena = new System.Windows.Forms.TextBox();
-            this.btn_Cerrar = new System.Windows.Forms.Button();
+            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lbl_usuario
             // 
             this.lbl_usuario.AutoSize = true;
             this.lbl_usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_usuario.Location = new System.Drawing.Point(14, 60);
+            this.lbl_usuario.ForeColor = System.Drawing.Color.Snow;
+            this.lbl_usuario.Location = new System.Drawing.Point(9, 96);
             this.lbl_usuario.Name = "lbl_usuario";
             this.lbl_usuario.Size = new System.Drawing.Size(54, 16);
             this.lbl_usuario.TabIndex = 0;
@@ -50,64 +53,93 @@
             // 
             this.lbl_pass.AutoSize = true;
             this.lbl_pass.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_pass.Location = new System.Drawing.Point(14, 129);
+            this.lbl_pass.ForeColor = System.Drawing.Color.Snow;
+            this.lbl_pass.Location = new System.Drawing.Point(9, 165);
             this.lbl_pass.Name = "lbl_pass";
             this.lbl_pass.Size = new System.Drawing.Size(76, 16);
             this.lbl_pass.TabIndex = 1;
             this.lbl_pass.Text = "Contraseña";
             // 
-            // btn_login
-            // 
-            this.btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_login.Location = new System.Drawing.Point(17, 204);
-            this.btn_login.Name = "btn_login";
-            this.btn_login.Size = new System.Drawing.Size(82, 23);
-            this.btn_login.TabIndex = 2;
-            this.btn_login.Text = "Login";
-            this.btn_login.UseVisualStyleBackColor = true;
-            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
-            // 
             // txt_Usuario
             // 
-            this.txt_Usuario.Location = new System.Drawing.Point(17, 79);
+            this.txt_Usuario.Location = new System.Drawing.Point(12, 115);
             this.txt_Usuario.Name = "txt_Usuario";
             this.txt_Usuario.Size = new System.Drawing.Size(226, 20);
             this.txt_Usuario.TabIndex = 3;
             // 
             // txt_Contrasena
             // 
-            this.txt_Contrasena.Location = new System.Drawing.Point(17, 148);
+            this.txt_Contrasena.Location = new System.Drawing.Point(12, 184);
             this.txt_Contrasena.Name = "txt_Contrasena";
             this.txt_Contrasena.PasswordChar = '*';
             this.txt_Contrasena.Size = new System.Drawing.Size(226, 20);
             this.txt_Contrasena.TabIndex = 4;
             // 
-            // btn_Cerrar
+            // iconButton1
             // 
-            this.btn_Cerrar.Location = new System.Drawing.Point(209, 273);
-            this.btn_Cerrar.Name = "btn_Cerrar";
-            this.btn_Cerrar.Size = new System.Drawing.Size(75, 23);
-            this.btn_Cerrar.TabIndex = 5;
-            this.btn_Cerrar.Text = "Cerrar";
-            this.btn_Cerrar.UseVisualStyleBackColor = true;
-            this.btn_Cerrar.Click += new System.EventHandler(this.btn_Cerrar_Click);
+            this.iconButton1.AutoEllipsis = true;
+            this.iconButton1.AutoSize = true;
+            this.iconButton1.BackColor = System.Drawing.Color.DarkGray;
+            this.iconButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconButton1.ForeColor = System.Drawing.Color.Black;
+            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.LockOpen;
+            this.iconButton1.IconColor = System.Drawing.Color.Black;
+            this.iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton1.IconSize = 25;
+            this.iconButton1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton1.Location = new System.Drawing.Point(88, 235);
+            this.iconButton1.Name = "iconButton1";
+            this.iconButton1.Size = new System.Drawing.Size(92, 34);
+            this.iconButton1.TabIndex = 5;
+            this.iconButton1.Text = "Login";
+            this.iconButton1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.iconButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.iconButton1.UseVisualStyleBackColor = false;
+            this.iconButton1.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // iconPictureBox1
+            // 
+            this.iconPictureBox1.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.iconPictureBox1.ForeColor = System.Drawing.Color.Silver;
+            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.Lock;
+            this.iconPictureBox1.IconColor = System.Drawing.Color.Silver;
+            this.iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconPictureBox1.Location = new System.Drawing.Point(12, 21);
+            this.iconPictureBox1.Name = "iconPictureBox1";
+            this.iconPictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.iconPictureBox1.TabIndex = 6;
+            this.iconPictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AllowDrop = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 33);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Insertar Usuario";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form_Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
             this.ClientSize = new System.Drawing.Size(285, 298);
-            this.Controls.Add(this.btn_Cerrar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.iconPictureBox1);
+            this.Controls.Add(this.iconButton1);
             this.Controls.Add(this.txt_Contrasena);
             this.Controls.Add(this.txt_Usuario);
-            this.Controls.Add(this.btn_login);
             this.Controls.Add(this.lbl_pass);
             this.Controls.Add(this.lbl_usuario);
-            this.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.ForeColor = System.Drawing.Color.Snow;
             this.Name = "Form_Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
             this.TransparencyKey = System.Drawing.Color.Black;
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,10 +149,11 @@
 
         private System.Windows.Forms.Label lbl_usuario;
         private System.Windows.Forms.Label lbl_pass;
-        private System.Windows.Forms.Button btn_login;
         private System.Windows.Forms.TextBox txt_Usuario;
         private System.Windows.Forms.TextBox txt_Contrasena;
-        private System.Windows.Forms.Button btn_Cerrar;
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
